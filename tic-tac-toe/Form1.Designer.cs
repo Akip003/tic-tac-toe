ea2f29aa91fb8d2575c62735bddc11cb9ed0eeea
Akip003 <grzywek123@gmail.com>
2022-08-07 19:00:40 +0200

    Winner functionality
    Added checking for winner
    Adder box displaying number of turns
    Added label notifying about the winner

--- tic-tac-toe/Form1.Designer.cs
+++ tic-tac-toe/Form1.Designer.cs
@@ -39,6 +39,8 @@
             this.button7 = new System.Windows.Forms.Button();
             this.button8 = new System.Windows.Forms.Button();
             this.button9 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.winnerLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // timer1
@@ -137,11 +139,37 @@
             this.button9.UseVisualStyleBackColor = true;
             this.button9.Click += new System.EventHandler(this.button9_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(665, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 25);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "label1";
+
+            // 
+            // winnerLabel
+            // 
+            this.winnerLabel.AutoSize = true;
+            this.winnerLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.winnerLabel.Location = new System.Drawing.Point(208, 180);
+            this.winnerLabel.Name = "winnerLabel";
+            this.winnerLabel.Size = new System.Drawing.Size(385, 76);
+            this.winnerLabel.TabIndex = 10;
+            this.winnerLabel.Text = "WINNER: X";
+            this.winnerLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.winnerLabel.Visible = false;
+            this.winnerLabel.Click += new System.EventHandler(this.winnerLabel_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 445);
+            this.Controls.Add(this.winnerLabel);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button9);
             this.Controls.Add(this.button8);
             this.Controls.Add(this.button7);
@@ -155,6 +183,7 @@
             this.Text = "Tic Tac Toe";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -171,6 +200,8 @@
         private System.Windows.Forms.Button button7;
         private System.Windows.Forms.Button button8;
         private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label winnerLabel;
     }
 }
 
--- tic-tac-toe/Form1.cs
+++ tic-tac-toe/Form1.cs
@@ -36,7 +36,7 @@ namespace tic_tac_toe
         Button[] ButtonArray;
 
 
-        void ButtonText(int btn)
+        void ButtonAction(int btn)
         {
             
             {
@@ -49,12 +49,42 @@ namespace tic_tac_toe
                         ButtonArray[btn - 1].Text = "O";
                     turn++;
                 }
-
+                CheckWin();
             }
            
         }
-        
-        
+        void CheckWin()
+        {
+            for (int j = 0; j <7; j+=3)
+                if (ButtonArray[j+1].Text== ButtonArray[j].Text && ButtonArray[j+2].Text== ButtonArray[j].Text)
+                {
+                    WriteWinner(j);
+                    
+                }
+           for(int j=0; j<3; j++)
+            {
+                if (ButtonArray[j].Text == ButtonArray[j+3].Text && ButtonArray[j].Text == ButtonArray[j+6].Text)
+                {
+                    WriteWinner(j);
+                }
+            }
+            if (ButtonArray[0].Text == ButtonArray[4].Text && ButtonArray[0].Text == ButtonArray[8].Text)
+            {
+                WriteWinner(0);
+            }
+            else if (ButtonArray[2].Text == ButtonArray[4].Text && ButtonArray[2].Text == ButtonArray[6].Text)
+            {
+                WriteWinner(2);
+            }
+        }
+        void WriteWinner(int j)
+        {
+            timer1.Stop();
+            Console.WriteLine(ButtonArray[j].Text + " Wins");
+            winnerLabel.Text = "WINNER: " + ButtonArray[j].Text;
+            winnerLabel.Visible = true;
+            return;
+        }
         
 
 
@@ -62,6 +92,7 @@ namespace tic_tac_toe
         private void timer1_Tick(object sender, EventArgs e)
         {
             Console.WriteLine(turn);
+            label1.Text = "Turn:\n   "+Convert.ToString(turn);
         }
 
        
@@ -73,47 +104,56 @@ namespace tic_tac_toe
 
         private void button1_Click(object sender, EventArgs e)
         {
-            ButtonText(1);
+            ButtonAction(1);
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            ButtonText(2);
+            ButtonAction(2);
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-            ButtonText(3);
+            ButtonAction(3);
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
-            ButtonText(4);
+            ButtonAction(4);
         }
 
         private void button5_Click(object sender, EventArgs e)
         {
-            ButtonText(5);
+            ButtonAction(5);
         }
 
         private void button6_Click(object sender, EventArgs e)
         {
-            ButtonText(6);
+            ButtonAction(6);
         }
 
         private void button7_Click(object sender, EventArgs e)
         {
-            ButtonText(7);
+            ButtonAction(7);
         }
 
         private void button8_Click(object sender, EventArgs e)
         {
-            ButtonText(8);
+            ButtonAction(8);
         }
 
         private void button9_Click(object sender, EventArgs e)
         {
-            ButtonText(9);
+            ButtonAction(9);
+            
+        }
+
+        
+
+
+        private void winnerLabel_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
